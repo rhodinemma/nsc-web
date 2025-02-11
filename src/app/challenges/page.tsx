48c7cf94f8ec2540fca3e67cd14e84c8071b74a8
rhodinemma <rhodinemma10@gmail.com>
2025-02-12 01:11:56 +0300

    feat: track completed and incomplete progress for challenges

--- src/app/challenges/page.tsx
+++ src/app/challenges/page.tsx
@@ -1,106 +1,119 @@
-/* eslint-disable react-hooks/rules-of-hooks */
 "use client";
-import React, { useState } from "react";
-
-import "./page.css";
+import React, { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
 import {
+  Box,
+  Card,
+  CardContent,
   Typography,
-  Divider,
   List,
   ListItemButton,
   ListItemIcon,
   ListItemText,
   Collapse,
-  Box,
-  Card,
-  CardContent,
+  Divider,
+  CircularProgress,
 } from "@mui/material";
-
-import CircularProgress, {
-  CircularProgressProps,
-} from "@mui/material/CircularProgress";
-
-import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
-
-import ExpandLess from "@mui/icons-material/ExpandLess";
-import ExpandMore from "@mui/icons-material/ExpandMore";
-import ExtensionIcon from "@mui/icons-material/Extension";
+import {
+  ExpandLess,
+  ExpandMore,
+  Extension as ExtensionIcon,
+  ArrowForwardIos as ArrowForwardIosIcon,
+} from "@mui/icons-material";
+import useParticipantStore from "@/store/participantStore";
 import Navbar from "@/components/Navbar";
-import { useRouter } from "next/navigation";
+
+interface Level {
+  id: string;
+  name: string;
+  description: string;
+  isCompleted: boolean;
+  path: string;
+}
 
 interface Game {
-  id: number;
+  id: string;
   name: string;
   description: string;
   isAuthorizedtoTake: boolean;
-  challenges: {
-    id: number;
-    name: string;
-    description: string;
-    isCompleted: boolean;
-    path: string;
-  }[];
+  challenges: Level[];
 }
 
-function CircularProgressWithLabel(
-  props: CircularProgressProps & { value: number }
-) {
+interface CompletedLevel {
+  levelId: string;
+  levelName: string;
+  score: number;
+}
+
+interface ChallengeProgress {
+  challengeId: string;
+  challengeName: string;
+  completedLevels: CompletedLevel[];
+}
+
+const CircularProgressWithLabel = ({ value }: { value: number }) => {
   return (
-    <Box sx={{ position: "relative", display: "inline-flex" }}>
-      <CircularProgress variant="determinate" {...props} />
+    <Box position="relative" display="inline-flex">
+      <CircularProgress variant="determinate" value={value} />
       <Box
-        sx={{
-          top: 0,
-          left: 0,
-          bottom: 0,
-          right: 0,
-          position: "absolute",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
+        top={0}
+        left={0}
+        bottom={0}
+        right={0}
+        position="absolute"
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
       >
-        <Typography
-          variant="caption"
-          component="div"
-          sx={{ color: "text.secondary" }}
-        >{`${Math.round(props.value)}%`}</Typography>
+        <Typography variant="caption" component="div" color="text.secondary">
+          {`${Math.round(value)}%`}
+        </Typography>
       </Box>
     </Box>
   );
-}
+};
 
 const Challenges = () => {
-  const games: Game[] = [
+  const { email } = useParticipantStore();
+
+  const [games, setGames] = useState<Game[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [mazeEscapeOpen, setmazeEscapeOpen] = useState<boolean>(false);
+  const [treasureHuntOpen, settreasureHuntOpen] = useState<boolean>(false);
+  const [patternPainterOpen, setpatternPainterOpen] = useState<boolean>(false);
+
+  const router = useRouter();
+
+  const baseGames: Game[] = [
     {
-      id: 1,
+      id: "672d1513bc573ddbaf73b560",
       name: "Maze Escape",
       description: "",
       isAuthorizedtoTake: true,
       challenges: [
         {
-          id: 1,
+          id: "672d1595bc573ddbaf73b562",
           name: "Level 1",
           description: "Level 1",
           isCompleted: false,
           path: "/maze-escape/level-one",
         },
         {
-          id: 2,
+          id: "672d177624ce330bc1ba79d3",
           name: "Level 2",
           description: "Level 2",
           isCompleted: false,
           path: "/maze-escape/level-two",
         },
         {
-          id: 3,
+          id: "672dd117eaef355fc63911e5",
           name: "Level 3",
           description: "Level 3",
           isCompleted: false,
           path: "/maze-escape/level-three",
         },
         {
-          id: 4,
+          id: "672dd11eeaef355fc63911e7",
           name: "Level 4",
           description: "Level 4",
           isCompleted: false,
@@ -109,76 +122,76 @@ const Challenges = () => {
       ],
     },
     {
-      id: 2,
+      id: "6748eb650a2fba264a22e700",
       name: "The bird",
       description: "",
       isAuthorizedtoTake: true,
       challenges: [
         {
-          id: 1,
+          id: "6748ecdf0a2fba264a22e702",
           name: "Level 1",
           description: "Level 1",
           isCompleted: false,
           path: "/bird/level-one",
         },
         {
-          id: 2,
+          id: "6748ed3f0a2fba264a22e704",
           name: "Level 2",
           description: "Level 2",
           isCompleted: false,
           path: "/bird/level-two",
         },
         {
-          id: 3,
+          id: "67912a6cbe66df3d39edf8a0",
           name: "Level 3",
           description: "Level 3",
           isCompleted: false,
           path: "/bird/level-three",
         },
         {
-          id: 4,
+          id: "67916472253ba1ccf5d603b6",
           name: "Level 4",
           description: "Level 4",
           isCompleted: false,
           path: "/bird/level-four",
         },
         {
-          id: 5,
+          id: "67916552253ba1ccf5d603b8",
           name: "Level 5",
           description: "Level 5",
           isCompleted: false,
           path: "/bird/level-five",
         },
         {
-          id: 6,
+          id: "679166f7253ba1ccf5d603ba",
           name: "Level 6",
           description: "Level 6",
           isCompleted: false,
           path: "/bird/level-six",
         },
         {
-          id: 7,
+          id: "679167f8253ba1ccf5d603bc",
           name: "Level 7",
           description: "Level 7",
           isCompleted: false,
           path: "/bird/level-seven",
         },
         {
-          id: 8,
+          id: "6791695c253ba1ccf5d603be",
           name: "Level 8",
           description: "Level 8",
           isCompleted: false,
           path: "/bird/level-eight",
         },
         {
-          id: 9,
+          id: "67916bc0253ba1ccf5d603c0",
           name: "Level 9",
           description: "Level 9",
           isCompleted: false,
           path: "/bird/level-nine",
         },
         {
-          id: 10,
+          id: "67916c8c253ba1ccf5d603c2",
           name: "Level 10",
           description: "Level 10",
           isCompleted: false,
@@ -187,129 +200,157 @@ const Challenges = () => {
       ],
     },
     {
-      id: 3,
-      name: "Pattern Painter",
+      id: "67ab69bb8663464f42566861",
+      name: "Turtle",
       description: "",
-      isAuthorizedtoTake: false,
+      isAuthorizedtoTake: true,
       challenges: [
         {
-          id: 1,
+          id: "67ab6a058663464f42566863",
           name: "Level 1",
           description: "Level 1",
           isCompleted: false,
-          path: "/pattern-painter/1",
+          path: "/turtle/level-one",
         },
         {
-          id: 2,
+          id: "67ab6a098663464f42566865",
           name: "Level 2",
           description: "Level 2",
           isCompleted: false,
-          path: "/pattern-painter/2",
+          path: "/turtle/level-two",
         },
         {
-          id: 3,
+          id: "67ab6a0c8663464f42566867",
           name: "Level 3",
           description: "Level 3",
           isCompleted: false,
-          path: "/pattern-painter/3",
-        },
-      ],
-    },
-    {
-      id: 4,
-      name: "Collect the stars",
-      description: "",
-      isAuthorizedtoTake: false,
-      challenges: [
-        {
-          id: 1,
-          name: "Level 1",
-          description: "Level 1",
-          isCompleted: false,
-          path: "/collect-the-stars/1",
+          path: "/turtle/level-three",
         },
         {
-          id: 2,
-          name: "Level 2",
-          description: "Level 2",
+          id: "67ab6a128663464f42566869",
+          name: "Level 4",
+          description: "Level 4",
           isCompleted: false,
-          path: "/collect-the-stars/2",
+          path: "/turtle/level-four",
         },
         {
-          id: 3,
-          name: "Level 3",
-          description: "Level 3",
+          id: "67ab6a158663464f4256686b",
+          name: "Level 5",
+          description: "Level 5",
           isCompleted: false,
-          path: "/collect-the-stars/3",
-        },
-      ],
-    },
-    {
-      id: 5,
-      name: "Robot Builder",
-      description: "",
-      isAuthorizedtoTake: false,
-      challenges: [
-        {
-          id: 1,
-          name: "Level 1",
-          description: "Level 1",
-          isCompleted: false,
-          path: "/robot-builder/1",
+          path: "/turtle/level-five",
         },
         {
-          id: 2,
-          name: "Level 2",
-          description: "Level 2",
+          id: "67ab6a188663464f4256686d",
+          name: "Level 6",
+          description: "Level 6",
           isCompleted: false,
-          path: "/robot-builder/2",
+          path: "/turtle/level-six",
         },
         {
-          id: 3,
-          name: "Level 3",
-          description: "Level 3",
+          id: "67ab6a1b8663464f4256686f",
+          name: "Level 7",
+          description: "Level 7",
           isCompleted: false,
-          path: "/robot-builder/3",
+          path: "/turtle/level-seven",
+        },
+        {
+          id: "67ab6a1f8663464f42566871",
+          name: "Level 8",
+          description: "Level 8",
+          isCompleted: false,
+          path: "/turtle/level-eight",
+        },
+        {
+          id: "67ab6a228663464f42566873",
+          name: "Level 9",
+          description: "Level 9",
+          isCompleted: false,
+          path: "/turtle/level-nine",
+        },
+        {
+          id: "67ab6a258663464f42566875",
+          name: "Level 10",
+          description: "Level 10",
+          isCompleted: false,
+          path: "/turtle/level-ten",
         },
       ],
     },
   ];
 
-  const gameactionMapper = (game_id: number): (() => void) => {
+  useEffect(() => {
+    const fetchAndUpdateProgress = async () => {
+      try {
+        setIsLoading(true);
+
+        const response = await fetch(
+          `http://localhost:3002/api/v1/progress/participant/${email}`
+        );
+        const data = await response.json();
+
+        if (data.status === "success") {
+          // Create a map of completed levelIds
+          const completedLevels = new Map();
+          data.data.challenges.forEach((challenge: ChallengeProgress) => {
+            challenge.completedLevels.forEach((level: CompletedLevel) => {
+              completedLevels.set(level.levelId, true);
+            });
+          });
+
+          // Update games with completion status
+          const updatedGames = baseGames.map((game) => ({
+            ...game,
+            challenges: game.challenges.map((challenge) => ({
+              ...challenge,
+              isCompleted: completedLevels.has(challenge.id),
+            })),
+          }));
+
+          setGames(updatedGames);
+        }
+      } catch (error) {
+        console.error("Error fetching progress:", error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    if (email) {
+      fetchAndUpdateProgress();
+    } else {
+      setGames(baseGames);
+      setIsLoading(false);
+    }
+  }, [email]);
+
+  const gameactionMapper = (game_id: string): (() => void) => {
     switch (game_id) {
-      case 1:
+      case "672d1513bc573ddbaf73b560":
         return () => setmazeEscapeOpen((prev) => !prev);
-      case 2:
+      case "6748eb650a2fba264a22e700":
         return () => settreasureHuntOpen((prev) => !prev);
-      case 3:
+      case "67ab69bb8663464f42566861":
         return () => setpatternPainterOpen((prev) => !prev);
-      case 4:
-        return () => setcollectTheStarsOpen((prev) => !prev);
-      case 5:
-        return () => setrobotBuilderOpen((prev) => !prev);
       default:
         return () => {};
     }
   };
 
-  const getgameactionmapper = (game_id: number): boolean => {
+  const getgameactionmapper = (game_id: string): boolean => {
     switch (game_id) {
-      case 1:
+      case "672d1513bc573ddbaf73b560":
         return mazeEscapeOpen;
-      case 2:
+      case "6748eb650a2fba264a22e700":
         return treasureHuntOpen;
-      case 3:
+      case "67ab69bb8663464f42566861":
         return patternPainterOpen;
-      case 4:
-        return collectTheStarsOpen;
-      case 5:
-        return robotBuilderOpen;
       default:
         return false;
     }
   };
 
-  const computeOverallScore = (game_id: number): number => {
+  const computeOverallScore = (game_id: string): number => {
     const game = games.find((g) => g.id === game_id);
     if (!game) return 0;
 
@@ -321,18 +362,22 @@ const Challenges = () => {
     return (completedChallenges / totalChallenges) * 100;
   };
 
-  const [mazeEscapeOpen, setmazeEscapeOpen] = useState<boolean>(false);
-  const [treasureHuntOpen, settreasureHuntOpen] = useState<boolean>(false);
-  const [patternPainterOpen, setpatternPainterOpen] = useState<boolean>(false);
-  const [collectTheStarsOpen, setcollectTheStarsOpen] =
-    useState<boolean>(false);
-  const [robotBuilderOpen, setrobotBuilderOpen] = useState<boolean>(false);
-
-  const router = useRouter();
-
   const handleNavigation = (path: string) => {
     router.push(`/challenges/${path}`);
   };
+
+  if (isLoading) {
+    return (
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        height="100vh"
+      >
+        <CircularProgress />
+      </Box>
+    );
+  }
 
   return (
     <>
@@ -405,12 +450,22 @@ const Challenges = () => {
                           <ListItemButton
                             key={idx}
                             sx={{ pl: 4 }}
+                            disabled={challenge.isCompleted}
                             onClick={() => handleNavigation(challenge.path)}
                           >
                             <ListItemIcon>
-                              <ExtensionIcon color="primary" />
+                              <ExtensionIcon
+                                color={
+                                  challenge.isCompleted ? "success" : "primary"
+                                }
+                              />
                             </ListItemIcon>
-                            <ListItemText primary={challenge.name} />
+                            <ListItemText
+                              primary={challenge.name}
+                              secondary={
+                                challenge.isCompleted ? "Completed" : null
+                              }
+                            />
                             <ListItemIcon>
                               <ArrowForwardIosIcon fontSize="small" />
                             </ListItemIcon>
